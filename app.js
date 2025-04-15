8ad7e5e04105122e4428d2cf4494e0de5968ad18
Rhibro <rhibron@gmail.com>
2025-04-15 15:16:15 +0200

    added mongo

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+    "mongodb+srv://rhibron:" + 
+    process.env.MONGO_ATLAS_PW + 
+    "@rest-shop.iokj0az.mongodb.net/?retryWrites=true&w=majority&appName=rest-shop",
+ );
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,6 +24,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
     }
+    next();
 });
 
 // routes
